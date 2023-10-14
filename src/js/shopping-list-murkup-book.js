45aacfbdae427a1b0efb88198c01bbd61e77ab85
NadiiaChernokur <chiernokur5@gmail.com>
2023-10-14 23:45:41 +0200

    svg

--- src/js/shopping-list-murkup-book.js
+++ src/js/shopping-list-murkup-book.js
@@ -10,64 +10,80 @@ window.addEventListener("load",  murkupBook);
 function murkupBook() {
    const list = localStorage.getItem("shopping-list")
    const listPars = JSON.parse(list)
-   let url = "";
-  
+   console.log(listPars.length)
+   
    listPars.map(({_id, buy_links,  author, title, description, book_image, list_name}) => {
-    buy_links.map(({url}) => {
-      url = url
-      console.log(url)
-     
-     })
-    murkup.insertAdjacentHTML("beforeend", `
-     <section class="container">
-<picture class="picture"> 
-<img class="img" src="${book_image}" alt="${title}" >
-</picture>
-<div class="book-description">
-<div class="remove-book"> 
 
-    <svg class="svg-button" >
-     <use href="./img/icons.svg#icon-dump" ></use>
-    </svg>
   
-</div>
-<h2 class="name">${title}</h2>
-<p class="category">${list_name}</p>
-<p class="description">${description}</p>
-
-<div class="svg">
-<p class="author">${author}</p>
-<div>
-  <ul class="platform">
-    <li class="platform-svg">
-      <a href="" target="_blank" rel="noopener noreferrer">
-      <img  class="icon" src="" alt="${title}" >
-      </a>
-    </li>
-  </ul>
-</div>
-</div>
-</div>
-</section>`)
-
-const removeButton = document.querySelector(".remove-book")
+      murkup.insertAdjacentHTML("beforeend", `
+      <section class="container">
+ <picture class="picture"> 
+ <img class="img" src="${book_image}" alt="${title}" >
+ </picture>
+ <div class="book-description">
+ <div class="remove-book"> 
+ 
+     <svg class="svg-button" >
+      <use href="./img/icons.svg#icon-dump" ></use>
+     </svg>
+   
+ </div>
+ <h2 class="name">${title}</h2>
+ <p class="category">${list_name}</p>
+ <p class="description">${description}</p>
+ 
+ <div class="svg">
+ <p class="author">${author}</p>
+ <div>
+   <ul class="platform">
+   <li class="platform-svg amazon">
+   <a href="${toGetUrl(buy_links)}" target="_blank" rel="noopener noreferrer">
+   <img src="../img/amazon.png" alt="" >
+   </a>
+   </li>
+   <li class="platform-svg book">
+   <a href="${toGetUrl(buy_links)}" target="_blank" rel="noopener noreferrer">
+   <img src="../img/book.png " alt="" >
+   </a>
+   
+ </li>
+   </ul>
+ </div>
+ </div>
+ </div>
+ </section>`)
+      
+ const removeButton = document.querySelector(".remove-book")
 removeButton.addEventListener ("click", toRemoveMurkup);
+    })
+   }
+  
+  
+   
 
 
-  })
 
-}
+
+
+
 
 
 function toRemoveMurkup() {
-  
+  console.log(this)
+  listPars.length -=1
   murkup.removeChild(document.querySelector('.container'));
   // murkup.classList.remove('container');
-  localStorage.removeItem("shopping-list")
+  // localStorage.removeItem("shopping-list")
 
 }
 
 
+function toGetUrl(evt) {
+  for (const e of evt) {
+    return e.url
+  }
+
+}
 
 
 
