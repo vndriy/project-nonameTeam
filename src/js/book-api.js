123a81bfa87cc8f04d39db625802f41f3d66f140
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-10-14 22:58:17 +1300

    rechange api.js

--- src/js/book-api.js
+++ src/js/book-api.js
@@ -11,13 +11,3 @@ export async function listOfCategories(keyword) {
     throw error;
   }
 }
-export async function getBookDetail(bookId) {
-  try {
-    const response = await axios.get(`${BASE_URL}/books/${bookId}`);
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-}
