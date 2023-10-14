616cecca8d713836763dc1421258f3bf0164809e
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-10-14 19:11:16 +1300

    add modal-book-selection, storage

--- src/js/book-api.js
+++ src/js/book-api.js
@@ -1,14 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = `https://books-backend.p.goit.global`;
 
 export async function listOfCategories(keyword) {
-    try {
-        const response = await axios.get(`${BASE_URL}/books/${keyword}`);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-
+  try {
+    const response = await axios.get(`${BASE_URL}/books/${keyword}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+export async function getBookDetail(bookId) {
+  try {
+    const response = await axios.get(`${BASE_URL}/books/${bookId}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
