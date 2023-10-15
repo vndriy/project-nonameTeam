edf34acab0d7c28d8c91f62feab061d186fdc845
DanyaLitvinov <vfvdaniel07@gmail.com>
2023-10-16 02:38:07 +0300

    add button up

---
+++ src/js/button-up.js
@@ -0,0 +1,29 @@
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    this.el.classList.remove('btn-up_hide');
+  },
+  hide() {
+    this.el.classList.add('btn-up_hide');
+  },
+  addEventListener() {      
+    window.addEventListener('scroll', () => {        
+      const scrollY = window.scrollY || document.documentElement.scrollTop;        
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    
+    document.querySelector('.btn-up').onclick = () => {        
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  btnUp.hide();
+});
+
+btnUp.addEventListener();
