663101c8c25ef2898df4434a98a14df07ecdd86e
NadiiaChernokur <chiernokur5@gmail.com>
2023-10-15 02:02:39 +0200

    js

--- src/js/shopping-list-murkup-book.js
+++ src/js/shopping-list-murkup-book.js
@@ -10,11 +10,11 @@ window.addEventListener("load",  murkupBook);
 function murkupBook() {
    const list = localStorage.getItem("shopping-list")
    const listPars = JSON.parse(list)
-   console.log(listPars.length)
+  //  console.log(listPars.length)
    
    listPars.map(({_id, buy_links,  author, title, description, book_image, list_name}) => {
-
-  
+    const applBook = buy_links[1]
+    
       murkup.insertAdjacentHTML("beforeend", `
       <section class="container">
  <picture class="picture"> 
@@ -24,7 +24,7 @@ function murkupBook() {
  <div class="remove-book"> 
  
      <svg class="svg-button" >
-      <use href="./img/icons.svg#icon-dump" ></use>
+      <use href="./img/icons.svg#icon-dump" class="${_id}"></use>
      </svg>
    
  </div>
@@ -42,7 +42,7 @@ function murkupBook() {
    </a>
    </li>
    <li class="platform-svg book">
-   <a href="${toGetUrl(buy_links)}" target="_blank" rel="noopener noreferrer">
+   <a href="${applBook.url}" target="_blank" rel="noopener noreferrer">
    <img src="../img/book.png " alt="" >
    </a>
    
@@ -52,32 +52,50 @@ function murkupBook() {
  </div>
  </div>
  </section>`)
-      
+//  console.log(_id)
+
+ function removeMurkup() {
+  const removeButton = document.querySelector(".remove-book")
+  removeButton.addEventListener ("click", toRemoveMurkup);
+ }
+ if(toRemoveMurkup) {
+
+ }
  const removeButton = document.querySelector(".remove-book")
 removeButton.addEventListener ("click", toRemoveMurkup);
     })
+    
    }
   
   
+ // Видаляємо книжку  
+function toRemoveMurkup(e) {
+  const list = localStorage.getItem("shopping-list")
+   const listPars = JSON.parse(list)
+   console.log(listPars)
+   listPars.map((b) => {
+    if(e.target.classList.value === b._id) {
+      localStorage.removeItem("shopping-list", "b")
+    }
+    // console.log(b)
+    // console.log(e.target.classList.value)
+   })
+  
+  // for (let i = 0; i < murkup.length; i--)
+  
+ 
+  // listPars.length -=1
+  
    
-
-
-
-
-
-
-
-
-function toRemoveMurkup() {
-  console.log(this)
-  listPars.length -=1
-  murkup.removeChild(document.querySelector('.container'));
+  
+  
+  // murkup.removeChild(document.querySelector('.container'));
   // murkup.classList.remove('container');
   // localStorage.removeItem("shopping-list")
 
 }
 
-
+// Отримуємо URL
 function toGetUrl(evt) {
   for (const e of evt) {
     return e.url
