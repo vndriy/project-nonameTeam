33b1db6af31c7dab32688740df0090d8e647dae7
fhlare <127776539+fhlare@users.noreply.github.com>
2023-10-19 02:28:07 +0300

    almost end support card

--- src/js/support-ua.js
+++ src/js/support-ua.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+import 'slick-carousel';
 import { supportArr } from './support-cards';
 
 const refs = {
@@ -7,75 +9,173 @@ const refs = {
   buttonUp: document.querySelector('.button-up'),
 };
 
+refs.list.insertAdjacentHTML('beforeend', renderSupport(supportArr));
+
+$('.js-support-list').slick({
+  vertical: true,
+  dots: false,
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  prevArrow: `<div></div>`,
+  nextArrow: `
+      <button type="button" class="scroll-list-button">
+        <svg class="button-scroll-ok" width="22" height="32">
+          <use href="./img/supportUkraine/icons.svg#icon-ok"></use>
+        </svg>
+      </button>
+    `,
+
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll:3,
+      },
+    },
+  ],
+});
+
 function renderSupport(supportArr) {
-refs.list.innerHTML = supportArr
-    .map(({ title, url, img }) => {
+const markup = supportArr
+  .map(({ title, url, img }, index) => {
+    const supportListNumber = String(index + 1).padStart(2, '0');
       return `
-      <li class="support-item">
-      <a class="support-link link" href="${url}" target="_blank" rel="noopener norferrer">
+      <div>
+      <div class="support-item">
+      <span class="support-number">${supportListNumber}</span><a class="support-link link" href="${url}" target="_blank" rel="noopener norferrer">
       <img class="img-support" src="${img}" alt=${title}/>
       </a>
-    </li>`;
+      </div>
+    </div>`;
     })
     .join('');
-
+  return markup;
 }
 
-renderSupport(supportArr);
 
-function supportListNumber() {
-  const supportNumber = document.querySelectorAll('.support-item');
-  supportNumber.forEach((item, index) => {
-    let number = (index + 1).toString().padStart(2, '0');
-    item.innerHTML = `<span class="support-number">${number}</span> ${item.innerHTML}`;
-  });
-}
 
-supportListNumber();
+// const options = {
+//   vertical: true,
+//   dots: false,
+//   infinite: true,
+//   slidesToShow: 6,
+//   slidesToScroll: 3,
+//   prevArrow: `<div></div>`,
+//   nextArrow: ` <div>
+//       <button type="button" class="scroll-list-button">
+//         <svg class="button-scroll-ok" width="22" height="32">
+//           <use href="./img/supportUkraine/icons.svg#icon-ok"></use>
+//         </svg>
+//       </button>
+//     </div>`,
 
-function visibleItems() {
-  const listItems = document.querySelectorAll('.support-item');
-  console.log(listItems);
-  for (let i = 0; i < listItems.length; i += 1){
-    if (i < 6) {
-      listItems[i].style.display = 'flex';
-    } else {
-      listItems[i].style.display = 'none';
-    }
-  }
-}
+//   responsive: [
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         slidesToShow: 6,
+//         slidesToScroll:3,
+//       },
+//     },
+//   ],
+// };
 
-visibleItems();
 
-refs.showAllBtn.addEventListener('click', onClickShowButtonAll);
 
-function onClickShowButtonAll () {
-  const listItems = document.querySelectorAll('.support-item');
-  listItems.forEach(item => {
-    item.style.display = 'flex';
-  });
 
-  refs.supportCard.style.maxHeight = 'none';
+// let areItemsVisible = true;
 
-  refs.showAllBtn.style.display = 'none';
-  refs.buttonUp.style.display = 'block';
-  refs.buttonUp.style.marginTop = "24px";
-  refs.buttonUp.style.marginBottom = "0";
+// function createSupportItems(supportArr, startIndex, endIndex) {
+//   supportListNumber();
+//   return supportArr
+//     .slice(startIndex, endIndex)
+//     .map(({ title, url, img }) => {
+//       return `
+//         <li class="support-item">
+//           <a class="support-link link" href="${url}" target="_blank" rel="noopener noreferrer">
+//             <img class="img-support" src="${img}" alt="${title}" />
+//           </a>
+//         </li>
+//       `;
+//     })
+//     .join('');
+// }
 
-  refs.showAllBtn.removeEventListener('click', onClickShowButtonAll);
-  refs.buttonUp.addEventListener('click', onClickButtonUp);
-}
+// function supportListNumber() {
+//   const supportNumber = document.querySelectorAll('.support-item');
+//   supportNumber.forEach((item, index) => {
+//     let number = (index + 1).toString().padStart(2, '0');
+//     item.innerHTML = `<span class="support-number">${number}</span> ${item.innerHTML}`;
+//   });
+// }
 
-function onClickButtonUp() {
-  if (window.innerWidth <= 767) {
-    refs.supportCard.style.maxHeight = "460px";
-  } else {
-    refs.supportCard.style.maxHeight = "474px";
-  }
+// supportListNumber();
 
-  visibleItems();
-  refs.buttonUp.style.display = 'none';
-  refs.showAllBtn.style.display = 'block';
-  refs.buttonUp.removeEventListener('click', onClickButtonUp);
-  refs.showAllBtn.addEventListener('click', onClickShowButtonAll);
-}
+// function renderSupport(supportArr, startIndex, endIndex) {
+//   const supportItemsHTML = createSupportItems(supportArr, startIndex, endIndex);
+//   refs.list.innerHTML = supportItemsHTML;
+// }
+
+// function toggleItemsVisibility() {
+//   const startIndex = areItemsVisible ? 0 : 3;
+//   const endIndex = areItemsVisible ? 6 : 9;
+
+//   renderSupport(supportArr, startIndex, endIndex);
+
+//   areItemsVisible = !areItemsVisible;
+
+// }
+
+// refs.showAllBtn.addEventListener('click', toggleItemsVisibility);
+
+// // Изначально отобразите первые 6 элементов
+// renderSupport(supportArr, 0, 6);
+
+// function visibleItems() {
+//   const listItems = document.querySelectorAll('.support-item');
+//   console.log(listItems);
+//   for (let i = 0; i < listItems.length; i += 1){
+//     if (i < 6) {
+//       listItems[i].style.display = 'flex';
+//     } else {
+//       listItems[i].style.display = 'none';
+//     }
+//   }
+// }
+
+// visibleItems();
+
+// refs.showAllBtn.addEventListener('click', onClickShowButtonAll);
+
+// function onClickShowButtonAll () {
+//   const listItems = document.querySelectorAll('.support-item');
+//   listItems.forEach(item => {
+//     item.style.display = 'flex';
+//   });
+
+//   refs.supportCard.style.maxHeight = 'none';
+
+//   refs.showAllBtn.style.display = 'none';
+//   refs.buttonUp.style.display = 'block';
+//   refs.buttonUp.style.marginTop = "24px";
+//   refs.buttonUp.style.marginBottom = "0";
+
+//   refs.showAllBtn.removeEventListener('click', onClickShowButtonAll);
+//   refs.buttonUp.addEventListener('click', onClickButtonUp);
+// }
+
+// function onClickButtonUp() {
+//   if (window.innerWidth <= 767) {
+//     refs.supportCard.style.maxHeight = "460px";
+//   } else {
+//     refs.supportCard.style.maxHeight = "474px";
+//   }
+
+//   visibleItems();
+//   refs.buttonUp.style.display = 'none';
+//   refs.showAllBtn.style.display = 'block';
+//   refs.buttonUp.removeEventListener('click', onClickButtonUp);
+//   refs.showAllBtn.addEventListener('click', onClickShowButtonAll);
+// }
